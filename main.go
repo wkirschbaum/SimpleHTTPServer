c8b229b590d7db3815c3b064815aecb4c6fdcce9
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2016-04-25 22:43:20 +0200

    Add colored logging

--- main.go
+++ main.go
@@ -5,25 +5,67 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	ip := getLocalIP()
-	port := os.Getenv("port")
-	if len(port) == 0 {
-		port = "8000"
-	}
+	ip := getLocalIP("0.0.0.0")
+	port := getPort("8000")
 
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
 	fmt.Printf("Listening on %s:%s\n", ip, port)
-	http.ListenAndServe(fmt.Sprintf("%s:%s", ip, port), nil)
+	listenString := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(listenString, logHandler(http.DefaultServeMux))
 }
 
-func getLocalIP() string {
+func logHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lw := &loggingResponseWriter{w, 0}
+		handler.ServeHTTP(lw, r)
+		if lw.status == 0 {
+			lw.status = 200
+		}
+		message := fmt.Sprintf("[%s] %d %s %s", stripPort(r.RemoteAddr), lw.status, r.Method, r.URL)
+
+		if lw.status <= 300 {
+			color.Green(message)
+		} else if lw.status <= 400 {
+			color.Cyan(message)
+		} else {
+			color.Red(message)
+		}
+	})
+}
+
+func stripPort(addr string) string {
+	return strings.Split(addr, ":")[0]
+}
+
+type loggingResponseWriter struct {
+	ResponseWriter http.ResponseWriter
+	status         int
+}
+
+func (w *loggingResponseWriter) WriteHeader(code int) {
+	w.status = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func (w *loggingResponseWriter) Write(b []byte) (int, error) {
+	return w.ResponseWriter.Write(b)
+}
+
+func (w loggingResponseWriter) Header() http.Header {
+	return w.ResponseWriter.Header()
+}
+
+func getLocalIP(fallback string) string {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return ""
+		return fallback
 	}
 	for _, address := range addrs {
 		// check the address type and if it is not a loopback the display it
@@ -33,5 +75,16 @@ func getLocalIP() string {
 			}
 		}
 	}
-	return ""
+	return fallback
+}
+
+func getPort(fallback string) string {
+	port := os.Getenv("port")
+	if len(port) == 0 {
+		port = os.Getenv("PORT")
+	}
+	if len(port) == 0 {
+		port = fallback
+	}
+	return port
 }
